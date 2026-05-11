4cdbd71c327ab5dd53d5ae1d627c0444f43d314b
Baraah Abdullah Ali Rfeqallah <baraah.rfeqallah@student.htw-berlin.de>
2026-05-11 19:48:51 +0000

    added Python file to create the map

---
+++ map.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import cgitb; cgitb.enable()
+import cgi
+import sys
+from tile import createMap, jsonFromFileOrPolygon
+
+query=cgi.FieldStorage()
+polygon = ("polygon" in query) and query['polygon'].value or None
+
+print("Content-Type:image/png\n")
+img = createMap(jsonFromFileOrPolygon(None, polygon))
+img.save(sys.stdout, "png")
+
