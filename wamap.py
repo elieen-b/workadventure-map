051bfa4e6cd41745b590cb246e024d1f261625b7
Baraah Rfeqallah <Baraah.Rfeqallah@student.htw-berlin.de>
2026-06-21 19:23:19 +0200

    edited road position

--- wamap.py
+++ wamap.py
@@ -123,8 +123,8 @@ def lonlat_to_pixel(lon, lat, bounds):
   canvas_height = mapHeight * 8
   x = int((lon - min_lon) / (max_lon - min_lon) * canvas_width)
   y = int((max_lat - lat) / (max_lat - min_lat) * canvas_height)
-  x += 55
-  y += 25
+  x += 50
+  y += 30
   return x, y
 
 def create_road_objects(osm_data, bounds):
